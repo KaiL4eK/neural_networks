cc08df39150ad2d6f595247dfd4785332445b7e3
KaiL4eK <darklexi494@gmail.com>
2017-04-28 12:55:10 +0300

    Added chinese sign and execute with picture
    Former-commit-id: 465aab40521c77aae2a613f688c921fa04b7c398
    Former-commit-id: 73e7714a0f4d64ef2a92177e14e714ba82f2e167

--- deconv/execute.py
+++ deconv/execute.py
@@ -14,9 +14,10 @@ import argparse
 # from skvideo.io import VideoCapture
 
 parser = argparse.ArgumentParser(description='Process video with ANN')
-parser.add_argument('videopath', action='store', help='Path to video file to process')
+parser.add_argument('filepath', action='store', help='Path to video file to process')
 parser.add_argument('weights', action='store', help='Path to weights file')
 parser.add_argument('-f', '--fps', action='store_true', help='Check fps')
+parser.add_argument('-p', '--pic', action='store_true', help='Process picture')
 
 args = parser.parse_args()
 
@@ -38,74 +39,103 @@ def get_bbox(frame, model):
 		return None
 
 def execute_model():
-	cap = cv2.VideoCapture(args.videopath)
-	if cap is None or not cap.isOpened():
-		print('Failed to open file')
+
+	if args.pic:
+		
+		frame = cv2.imread(args.filepath)
+		if frame is None:
+			print('Failed to open file')
+			exit(1)
+
+		f_height, f_width, f_cahnnels = frame.shape
+		scale_x = float(f_width)/nn_out_size
+		scale_y = float(f_height)/nn_out_size
+
+		model = get_unet()
+		print_summary(model)
+		model.load_weights(args.weights)
+
+		bbox = get_bbox(frame, model)
+
+		if bbox:
+			# print(bbox)
+			bbox = [int(i) for i in np.multiply(bbox, [scale_x, scale_y, scale_x, scale_y])]
+			# print(bbox)
+			R.draw_rects(frame, [bbox])
+		
+		cv2.imshow('frame',frame)
+		cv2.waitKey(0)
 		exit(1)
+	else:
 
-	ret, frame = cap.read()
-	if frame is None:
-		print('Failed to read frame')
-		exit(1)
+		cap = cv2.VideoCapture(args.filepath)
+		if cap is None or not cap.isOpened():
+			print('Failed to open file')
+			exit(1)
 
-	f_height, f_width, f_cahnnels = frame.shape
-	scale_x = float(f_width)/nn_out_size
-	scale_y = float(f_height)/nn_out_size
+		ret, frame = cap.read()
+		if frame is None:
+			print('Failed to read frame')
+			exit(1)
 
-	# model = load_model('test_model.h5', custom_objects={'iou_loss':iou_loss})
-	model = get_unet()
-	print_summary(model)
-	model.load_weights(args.weights)
+		f_height, f_width, f_cahnnels = frame.shape
+		scale_x = float(f_width)/nn_out_size
+		scale_y = float(f_height)/nn_out_size
 
-	if args.fps:
-		bbox_obtain_time = 0
-		num_frames = 120
-		start = time.time()
-		for i in xrange(0, num_frames) :
-			ret, frame = cap.read()
-			if frame is None:
-				exit(1)
+		# model = load_model('test_model.h5', custom_objects={'iou_loss':iou_loss})
+		model = get_unet()
+		print_summary(model)
+		model.load_weights(args.weights)
 
-			start_bbox = time.time()
-			bbox = get_bbox(frame, model)
-			bbox_obtain_time += (time.time() - start_bbox)
+		if args.fps:
+			bbox_obtain_time = 0
+			num_frames = 120
+			start = time.time()
+			for i in xrange(0, num_frames) :
+				ret, frame = cap.read()
+				if frame is None:
+					exit(1)
 
-			if bbox:
-				# print(bbox)
-				bbox = [int(i) for i in np.multiply(bbox, [scale_x, scale_y, scale_x, scale_y])]
-				# print(bbox)
-				R.draw_rects(frame, [bbox])
-			
-			cv2.imshow('frame',frame)
-			if cv2.waitKey(1) & 0xFF == ord('q'):
-				break
+				start_bbox = time.time()
+				bbox = get_bbox(frame, model)
+				bbox_obtain_time += (time.time() - start_bbox)
 
-		seconds = time.time() - start
-		print('Mean time per frame : {0} ms'.format(seconds / num_frames * 1000))
-		print('Estimated frames per second : {0}'.format(num_frames / seconds))
-		print('Bbox obtain mean time : {0} ms'.format(bbox_obtain_time / num_frames * 1000))
+				if bbox:
+					# print(bbox)
+					bbox = [int(i) for i in np.multiply(bbox, [scale_x, scale_y, scale_x, scale_y])]
+					# print(bbox)
+					R.draw_rects(frame, [bbox])
+				
+				cv2.imshow('frame',frame)
+				if cv2.waitKey(1) & 0xFF == ord('q'):
+					break
 
-	else:	
-		while(cap.isOpened()):
-			ret, frame = cap.read()
-			if frame is None:
-				exit(1)
+			seconds = time.time() - start
+			print('Mean time per frame : {0} ms'.format(seconds / num_frames * 1000))
+			print('Estimated frames per second : {0}'.format(num_frames / seconds))
+			print('Bbox obtain mean time : {0} ms'.format(bbox_obtain_time / num_frames * 1000))
 
-			# frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-			bbox = get_bbox(frame, model)
+		else:	
+			while(cap.isOpened()):
+				ret, frame = cap.read()
+				if frame is None:
+					exit(1)
 
-			if bbox:
-				# print(bbox)
-				bbox = [int(i) for i in np.multiply(bbox, [scale_x, scale_y, scale_x, scale_y])]
-				# print(bbox)
-				R.draw_rects(frame, [bbox])
-			
-			cv2.imshow('frame',frame)
-			if cv2.waitKey(1) & 0xFF == ord('q'):
-				break
+				# frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+				bbox = get_bbox(frame, model)
 
-	cap.release()
-	cv2.destroyAllWindows()
+				if bbox:
+					# print(bbox)
+					bbox = [int(i) for i in np.multiply(bbox, [scale_x, scale_y, scale_x, scale_y])]
+					# print(bbox)
+					R.draw_rects(frame, [bbox])
+				
+				cv2.imshow('frame',frame)
+				if cv2.waitKey(1) & 0xFF == ord('q'):
+					break
+
+		cap.release()
+		cv2.destroyAllWindows()
 
 if __name__ == '__main__':
 	execute_model()
