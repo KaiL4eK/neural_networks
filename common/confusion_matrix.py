7a417143130de2bdcc270c6f8d733768fc8fb162
KaiL4eK <darklexi494@gmail.com>
2017-08-06 00:58:29 +0300

    +u Confusion matrix develop
    Former-commit-id: 7345c7460d8d9352671d3ad552ad320149d38f56
    Former-commit-id: 7cdd7136c4e4641fb65f9a7a98445e57a50450d3
    Former-commit-id: 2b2461dea0453374da0534c9cbaae071a1c4230a
    Former-commit-id: 200b268b22f0d5c7daaacc9bc2ec1ce834b83922 [formerly 1bc25692af3a697237f0031acfad9af58962c9db]
    Former-commit-id: c4f10580eab92b5fa8cf082000fe24abab966096

--- common/confusion_matrix.py
+++ common/confusion_matrix.py
@@ -12,10 +12,14 @@ import numpy as np
 
 class ConfusionMatrix:
 	def __init__ (self):
-		matrix = np.ndarray((4, 4), dtype=np.uint8)
+		self.matrix = np.ndarray((4, 4), dtype=np.uint8)
+		self.reset_metrics();
 
 	def append_sample (self, truth, predicted):
 		if truth not in [0, 1] or predicted not in [0, 1]:
 			raise ValueError('Ground thruth or predicted values must be 0 or 1')
 
+		matrix[truth, predicted] += 1
 
+	def reset_metrics(self):
+		self.matrix.fill(0)
