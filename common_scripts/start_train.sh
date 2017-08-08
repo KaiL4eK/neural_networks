b77b64705292817423843008dffb4cd58d1ece48
KaiL4eK <darklexi494@gmail.com>
2017-08-08 23:41:19 +0300

    +u new net with weights
    Former-commit-id: 788f43b70f9706f227ec85b8ef41289f0e038fe5
    Former-commit-id: 1bda878be93d38a8026eefa5ad94bf8c92d08f5c
    Former-commit-id: edac6715c6edfef3dda08309c4ead7adb44f0d2e
    Former-commit-id: b003bc2cffe6890467bd595f8cf1b493a0e048d1 [formerly ebbfbac65a631830d5d3597d6ab979594e1dc0a9]
    Former-commit-id: b9d97bdd4fbbc10be40d5a559848ed0fedf62842

--- common_scripts/start_train.sh
+++ common_scripts/start_train.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#python train.py -w last_weights_best.h5 2>&1 | tee learn.log
-python train.py -w weights_best.h5 -b 30 2>&1 | tee learn.log
-#python train.py 2>&1 | tee learn.log
+python train.py -w weights_best.h5 -ab 30 -l 1e-4 2>&1 | tee learn.log
