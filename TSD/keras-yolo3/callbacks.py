1bea156a50690ae3e4848f6fd099cd8afd4e89a0
KaiL4eK <darklexi494@gmail.com>
2019-03-03 13:01:58 +0300

    [TSD/yolo] another attempt

--- TSD/keras-yolo3/callbacks.py
+++ TSD/keras-yolo3/callbacks.py
@@ -5,6 +5,52 @@ import keras
 from utils.utils import evaluate
 
 
+class CustomModelCheckpoint(ModelCheckpoint):
+    """ to save the template model, not the multi-GPU model
+    """
+
+    def __init__(self, model_to_save, **kwargs):
+        super(CustomModelCheckpoint, self).__init__(**kwargs)
+        self.model_to_save = model_to_save
+
+    def on_epoch_end(self, epoch, logs={}):
+
+        self.epochs_since_last_save += 1
+        if self.epochs_since_last_save >= self.period:
+            self.epochs_since_last_save = 0
+            filepath = self.filepath.format(epoch=epoch + 1, **logs)
+            if self.save_best_only:
+                current = logs.get(self.monitor)
+                if current is None:
+                    warnings.warn('Can save best model only with %s available, '
+                                  'skipping.' % (self.monitor), RuntimeWarning)
+                else:
+                    if self.monitor_op(current, self.best):
+                        if self.verbose > 0:
+                            print('\nEpoch %05d: %s improved from %0.5f to %0.5f,'
+                                  ' saving model to %s'
+                                  % (epoch + 1, self.monitor, self.best,
+                                     current, filepath))
+                        self.best = current
+                        if self.save_weights_only:
+                            self.model_to_save.save_weights(filepath, overwrite=True)
+                        else:
+                            self.model_to_save.save(filepath, overwrite=True)
+                    else:
+                        if self.verbose > 0:
+                            print('\nEpoch %05d: %s did not improve from %0.5f' %
+                                  (epoch + 1, self.monitor, self.best))
+            else:
+                if self.verbose > 0:
+                    print('\nEpoch %05d: saving model to %s' % (epoch + 1, filepath))
+                if self.save_weights_only:
+                    self.model_to_save.save_weights(filepath, overwrite=True)
+                else:
+                    self.model_to_save.save(filepath, overwrite=True)
+
+        super(CustomModelCheckpoint, self).on_batch_end(epoch, logs)
+
+
 class MAP_evaluation(keras.callbacks.Callback):
     """ Evaluate a given dataset using a given model.
             code originally from https://github.com/fizyr/keras-retinanet
@@ -28,7 +74,8 @@ class MAP_evaluation(keras.callbacks.Callback):
                  period=1,
                  save_best=False,
                  save_name=None,
-                 tensorboard=None):
+                 tensorboard=None,
+                 infer_sz=[416, 416]):
 
         self.infer_model = infer_model
         self.generator = generator
@@ -38,20 +85,21 @@ class MAP_evaluation(keras.callbacks.Callback):
         self.save_best = save_best
         self.save_name_fmt = save_name
         self.tensorboard = tensorboard
+        self.infer_sz = infer_sz
 
         self.bestMap = 0
 
         if not isinstance(self.tensorboard, keras.callbacks.TensorBoard) and self.tensorboard is not None:
             raise ValueError("Tensorboard object must be a instance from keras.callbacks.TensorBoard")
 
-    def on_epoch_end(self, epoch, logs=None):
+    def on_epoch_end(self, epoch, logs={}):
 
         if epoch % self.period == 0 and self.period != 0:
             average_precisions = evaluate(model=self.infer_model,
                                           generator=self.generator,
                                           iou_threshold=self.iou_threshold,
-                                          net_h=416,
-                                          net_w=416,
+                                          net_h=self.infer_sz[0],
+                                          net_w=self.infer_sz[1],
                                           save_path=None)
             print('\n')
             for label, average_precision in average_precisions.items():
@@ -100,49 +148,3 @@ class CustomTensorBoard(TensorBoard):
             self.writer.flush()
 
         super(CustomTensorBoard, self).on_batch_end(batch, logs)
-
-
-class CustomModelCheckpoint(ModelCheckpoint):
-    """ to save the template model, not the multi-GPU model
-    """
-
-    def __init__(self, model_to_save, **kwargs):
-        super(CustomModelCheckpoint, self).__init__(**kwargs)
-        self.model_to_save = model_to_save
-
-    def on_epoch_end(self, epoch, logs=None):
-        logs = logs or {}
-        self.epochs_since_last_save += 1
-        if self.epochs_since_last_save >= self.period:
-            self.epochs_since_last_save = 0
-            filepath = self.filepath.format(epoch=epoch + 1, **logs)
-            if self.save_best_only:
-                current = logs.get(self.monitor)
-                if current is None:
-                    warnings.warn('Can save best model only with %s available, '
-                                  'skipping.' % (self.monitor), RuntimeWarning)
-                else:
-                    if self.monitor_op(current, self.best):
-                        if self.verbose > 0:
-                            print('\nEpoch %05d: %s improved from %0.5f to %0.5f,'
-                                  ' saving model to %s'
-                                  % (epoch + 1, self.monitor, self.best,
-                                     current, filepath))
-                        self.best = current
-                        if self.save_weights_only:
-                            self.model_to_save.save_weights(filepath, overwrite=True)
-                        else:
-                            self.model_to_save.save(filepath, overwrite=True)
-                    else:
-                        if self.verbose > 0:
-                            print('\nEpoch %05d: %s did not improve from %0.5f' %
-                                  (epoch + 1, self.monitor, self.best))
-            else:
-                if self.verbose > 0:
-                    print('\nEpoch %05d: saving model to %s' % (epoch + 1, filepath))
-                if self.save_weights_only:
-                    self.model_to_save.save_weights(filepath, overwrite=True)
-                else:
-                    self.model_to_save.save(filepath, overwrite=True)
-
-        super(CustomModelCheckpoint, self).on_batch_end(epoch, logs)
