97260ddebcf296b22b73613378b4b4e4c01b3447
KaiL4eK <darklexi494@gmail.com>
2019-03-14 02:59:20 +0300

    [TSD/yolo] Last fix on ROS YOLO inference

--- TSD/keras-yolo3/utils/yolo.py
+++ TSD/keras-yolo3/utils/yolo.py
@@ -6,27 +6,25 @@ class YOLO:
         self.model = model
         self.net_sz = [net_h, net_w]
         self.anchors = anchors
+        self.anchor_count = len(self.anchors) // 2
+
         self.obj_thresh = obj_thresh
         self.nms_thresh = nms_thresh
 
-    def infer(self, model, images, net_h, net_w, anchors, obj_thresh, nms_thresh):
+    def make_infer(self, images):
         # anchors - raw format
 
         image_h, image_w, _ = images[0].shape
         nb_images = len(images)
-        batch_input = np.zeros((nb_images, net_h, net_w, 3))
-
-        anchor_count = len(anchors) // 2
+        batch_input = np.zeros((nb_images, self.net_sz[0], self.net_sz[1], 3))
 
         for i in range(nb_images):
-            batch_input[i] = preprocess_input(images[i], net_h, net_w)
+            batch_input[i] = preprocess_input(images[i], self.net_sz[0], self.net_sz[1])
 
-        batch_output = model.predict_on_batch(batch_input)
+        batch_output = self.model.predict_on_batch(batch_input)
         batch_boxes = [None] * nb_images
 
         net_output_count = len(batch_output)
-
-        assert net_output_count * 3 == anchor_count, 'Invalid achors count'
 
         for i in range(nb_images):
 
@@ -41,13 +39,12 @@ class YOLO:
                 l_idx = net_output_count - 1
                 r_idx = net_output_count
 
-                yolo_anchors = anchors[(l_idx - j) * 6:(r_idx - j) * 6]
+                yolo_anchors = self.anchors[(l_idx - j) * 6:(r_idx - j) * 6]
 
-                boxes += decode_netout(yolos[j], yolo_anchors, obj_thresh, net_h, net_w)
+                boxes += decode_netout(yolos[j], yolo_anchors, self.obj_thresh, self.net_sz[0], self.net_sz[1])
 
-            correct_yolo_boxes(boxes, image_h, image_w, net_h, net_w)
-            do_nms(boxes, nms_thresh)
+            correct_yolo_boxes(boxes, image_h, image_w, self.net_sz[0], self.net_sz[1])
+            do_nms(boxes, self.nms_thresh)
             batch_boxes[i] = boxes
 
         return batch_boxes
-
