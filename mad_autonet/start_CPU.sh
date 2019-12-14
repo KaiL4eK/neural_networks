2d6bf7c3a9df93df32c8bc3c14d9da1384ed2c94
AlexSAU <alex_sau@etu.ru>
2019-12-14 17:38:55 +0300

    [mad_autonet] Removed model

--- mad_autonet/start_CPU.sh
+++ mad_autonet/start_CPU.sh
@@ -3,5 +3,5 @@
 BASE_NET=MbN2_416x416_t1
 
 ./build/app -d CPU -i test.jpg \
-    -r ../TSD/src/_gen/ir_models/"$BASE_NET"_FP16.xml \
-    -c ../TSD/src/_gen/ir_models/$BASE_NET.json
+    -r models/"$BASE_NET"_FP16.xml \
+    -c models/$BASE_NET.json
