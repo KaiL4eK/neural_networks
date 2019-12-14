0ae58d85c0fdc328eba02434370278b9fffda423
AlexSAU <alex_sau@etu.ru>
2019-12-14 16:40:56 +0300

    [Autonet] Application for detection

--- openvino_autonet/src/yolo_ov.cpp
+++ openvino_autonet/src/yolo_ov.cpp
@@ -134,72 +134,12 @@ void YOLO_OpenVINO::infer(cv::Mat raw_image, vector<DetectionObject> &detections
             const ie::SizeVector &output_dims = output_blob->getTensorDesc().getDims();
 
             vector<cv::Point> anchors = get_anchors(i_layer);
-
+            
             const float grid_w = output_dims[3];
             const float grid_h = output_dims[2];
             const size_t chnl_count = output_dims[1];
 
-            const float *detection = static_cast<ie::PrecisionTrait<ie::Precision::FP32>::value_type *>(output_blob->buffer());
-            const size_t b_stride = (output_dims[1] * output_dims[2] * output_dims[3]);
-            const size_t c_stride = (output_dims[2] * output_dims[3]);
-            const size_t h_stride = output_dims[3];
-            size_t c_idx;
-
-            const size_t class_count = output_dims[1] / anchors.size() - 5;
-            const size_t box_count = class_count + 5;
-
-            float obj_thresh = mCfg._objectness_thresh;
-
-            for (size_t b_idx = 0; b_idx < output_dims[0]; b_idx++)
-            {
-                for (size_t h_idx = 0; h_idx < output_dims[2]; h_idx++)
-                {
-                    for (size_t w_idx = 0; w_idx < output_dims[3]; w_idx++)
-                    {
-                        size_t grid_offset = b_idx * b_stride + h_idx * h_stride + w_idx;
-
-                        for (size_t anc_idx = 0; anc_idx < anchors.size(); anc_idx++)
-                        {
-                            RawDetectionObject det;
-                            size_t chnl_offset = anc_idx * box_count;
-
-                            // size_t box_idx_x = 0;
-                            // size_t box_idx_y = 1;
-                            // size_t box_idx_w = 2;
-                            // size_t box_idx_h = 3;
-                            // size_t obj_idx = 4;
-                            // size_t cls_idx = 5;
-
-                            float obj = detection[grid_offset + c_stride * (4 + chnl_offset)];
-                            obj = sigmoid(obj);
-                            if (obj < obj_thresh)
-                                continue;
-
-                            det.x = detection[grid_offset + c_stride * (0 + chnl_offset)];
-                            det.y = detection[grid_offset + c_stride * (1 + chnl_offset)];
-                            det.w = detection[grid_offset + c_stride * (2 + chnl_offset)];
-                            det.h = detection[grid_offset + c_stride * (3 + chnl_offset)];
-                            
-                            det.w = anchors[anc_idx].x * exp(det.w) / mCfg._infer_sz.width;
-                            det.h = anchors[anc_idx].y * exp(det.h) / mCfg._infer_sz.height;
-                            det.x = (sigmoid(det.x) + w_idx) / grid_w;
-                            det.y = (sigmoid(det.y) + h_idx) / grid_h;
-
-                            for (size_t i_cls = 0; i_cls < class_count; i_cls++)
-                            {
-                                float class_val = detection[grid_offset + c_stride * ((i_cls + 5) + chnl_offset)];
-                                det.conf = sigmoid(class_val) * obj;
-
-                                if ( det.conf < obj_thresh )
-                                    continue;
-                                
-                                det.cls_idx = i_cls;
-                                global_dets.push_back(det);
-                            }
-                        }
-                    }
-                }
-            }
+            get_detections(global_dets, output_blob->buffer().as<void *>(), grid_h, grid_w, chnl_count, anchors, ParsingFormat::CHW);
         }
 
         rsz_cfg.tile_idx = i;
