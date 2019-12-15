3021fbcf47d386b9aa68720cd441580fe45fcaa9
KaiL4eK <darklexi494@gmail.com>
2019-12-15 15:36:40 +0300

    Detection app moved to special repo

--- mad_autonet/install-deps.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo apt install libboost1.65-all-dev
--- mad_autonet/start_CPU.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BASE_NET=MbN2_416x416_t1
-
-./build/app -d CPU -i test.jpg \
-    -r models/"$BASE_NET"_FP16.xml \
-    -c models/$BASE_NET.json
