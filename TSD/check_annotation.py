61fcab61ed749af8aea849445c0077302846b586
KaiL4eK <darklexi494@gmail.com>
2020-06-10 11:15:00 +0300

    Last fixes

--- TSD/check_annotation.py
+++ TSD/check_annotation.py
@@ -2,14 +2,40 @@
 import os
 from pathlib import Path
 
-ANNOT_DIR = '/home/alexey/data/NN_experiments/TSD/data/RF/TSD/RF19/Annotations/'
-IMAGES_DIR = '/home/alexey/data/NN_experiments/TSD/data/RF/TSD/RF19/Images/'
+ANNOT_DIRS = [
+    '/home/alexey/data/NN_experiments/TSD/data/RF/TSD/RF17/Annotations/',
+    '/home/alexey/data/NN_experiments/TSD/data/RF/TSD/RF19/Annotations/',
+]
 
-annot_fstems = [Path(fname).stem for fname in os.listdir(ANNOT_DIR) if fname.endswith('.xml')]
-images_fstems = [Path(fname).stem for fname in os.listdir(IMAGES_DIR)]
+IMAGES_DIRS = [
+    '/home/alexey/data/NN_experiments/TSD/data/RF/TSD/RF17/Images/',
+    '/home/alexey/data/NN_experiments/TSD/data/RF/TSD/RF19/Images/'
+]
+
+annot_fstems = []
+for ann_dir in ANNOT_DIRS:
+    for fname in os.listdir(ann_dir):
+        annot_fstems.append(
+            Path(fname).stem
+        )
+
+images_fstems = []
+for img_dir in IMAGES_DIRS:
+    for fname in os.listdir(img_dir):
+        images_fstems.append(
+            Path(fname).stem
+        )
 
 imgs_wo_annots = set(images_fstems) - (set(images_fstems) & set(annot_fstems))
 anns_wo_images = set(annot_fstems) - (set(images_fstems) & set(annot_fstems))
 
 print(imgs_wo_annots)
 print(anns_wo_images)
+
+for ann in anns_wo_images:
+    for ann_dir in ANNOT_DIRS:
+        try:
+            pass
+            os.remove(os.path.join(ann_dir, ann+'.xml'))
+        except:
+            pass
